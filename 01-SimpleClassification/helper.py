e1f31110adafbec7ef4ef19f6aa914829d55dc9f
juned <juned@juned-ThinkPad-Twist.(none)>
2013-10-13 22:45:19 +0530

    adkj

--- 01-SimpleClassification/helper.py
+++ 01-SimpleClassification/helper.py
@@ -1,6 +1,16 @@
 import numpy
 import pylab
-
+import csv
+def dataFromFile(fileName):
+    dataList=[]
+    with open(fileName, 'rb') as csvfile:
+        rows = csv.reader(csvfile)
+        for row in rows:
+            frow=[]
+            for d in row:
+                frow.append(float(d))
+            dataList.append(frow)
+    return dataList
 def visualize(trainedsvm,data,labels,axis_range,nSamplePoint):
     xx=numpy.linspace(axis_range[0][0],axis_range[0][1],nSamplePoint)
     yy=numpy.linspace(axis_range[1][0],axis_range[1][1],nSamplePoint)
@@ -8,7 +18,6 @@ def visualize(trainedsvm,data,labels,axis_range,nSamplePoint):
     Z=trainedsvm.predict(numpy.c_[xx.ravel(),yy.ravel()])
     Z=Z.reshape(xx.shape)
     pylab.contourf(xx,yy,Z)
-    
     color_map = { 0: (0, 0, .9), 1: (1, 0, 0)}
     colors = [color_map[y] for y in labels]
     pylab.scatter(data[:, 0], data[:, 1], c=colors, cmap=pylab.cm.Paired)
--- 01-SimpleClassification/tutorial1.py
+++ 01-SimpleClassification/tutorial1.py
@@ -1,12 +1,27 @@
 from sklearn import svm
 import numpy 
 import pylab
-from helper import visualize
+from helper import visualize ,dataFromFile
 
-X=numpy.array([[0,0],[1,1],[1,0],[0,1]])
-Y=[0,1,1,1]
+obj1=dataFromFile('object1.txt')
+obj2=dataFromFile('object2.txt')
+label1= [1]*len(obj1)
+label2=[0]*len(obj2)
+
+trainingData=numpy.array( obj1+obj2)
+labels=numpy.array(label1+label2)
+
 svc=svm.SVC(C=1.0,kernel='linear')
-trainedsvm=svc.fit(X,Y)
-visualize(trainedsvm,X,Y,[[-1.5,1.5],[-1.5,1.5]],100)
-print trainedsvm.predict([0,0])
-print trainedsvm.predict([1,0])
+trainedsvm=svc.fit(trainingData,labels)
+
+visualize(trainedsvm,trainingData,labels,[[-1,25],[-1,25]],100)
+
+#X=numpy.array([[0,0],[1,1],[1,0],[0,1]])
+#Y=[0,1,1,1]
+#trainedsvm=svc.fit(X,Y)
+#print "True Label for [0,0] :" ,0, " Predicted :" ,trainedsvm.predict([0,0])
+#print "True Label for [1,0:" ,1, " Predicted :" ,trainedsvm.predict([1,0])
+
+#visualize(trainedsvm,X,Y,[[-1.5,1.5],[-1.5,1.5]],100)
+
+#sample_weight=[2,1,1,1]
